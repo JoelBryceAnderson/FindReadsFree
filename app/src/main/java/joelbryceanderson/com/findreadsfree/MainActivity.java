00db12c2a4262056461979c0138cb9470adacc32
Joel Anderson <joelbryceanderson@gmail.com>
2017-02-08 17:26:37 -0500

    added loading of info into views, tab switching

--- app/src/main/java/joelbryceanderson/com/findreadsfree/MainActivity.java
+++ app/src/main/java/joelbryceanderson/com/findreadsfree/MainActivity.java
@@ -1,11 +1,18 @@
 package joelbryceanderson.com.findreadsfree;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
+import joelbryceanderson.com.findreadsfree.models.Page;
 import joelbryceanderson.com.findreadsfree.services.BackendService;
 import joelbryceanderson.com.findreadsfree.services.ServiceFactory;
 import rx.android.schedulers.AndroidSchedulers;
@@ -17,15 +24,48 @@ public class MainActivity extends AppCompatActivity {
 
     BackendService mBackendService;
 
+    Page mFree;
+    Page mDiscounted;
+    Page mAudioBook;
+
+    TextView mDescription;
+    TextView mPageTitle;
+
+    BottomNavigationView mBottomNav;
+
+    String mReferralLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView) findViewById(R.id.book_description);
-        tv.setMovementMethod(new ScrollingMovementMethod());
+        mPageTitle = (TextView) findViewById(R.id.page_title);
+        mDescription = (TextView) findViewById(R.id.book_description);
+        mDescription.setMovementMethod(new ScrollingMovementMethod());
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(onBottomNavSelected());
 
         loadPages();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onBottomNavSelected() {
+        return item -> {
+            switch (item.getItemId()) {
+                case R.id.action_free:
+                    showPage(mFree, getString(R.string.section_title_free));
+                    break;
+                case R.id.action_promotion:
+                    showPage(mDiscounted, getString(R.string.section_title_discounted));
+                    break;
+                case R.id.action_audio_book:
+                    showPage(mAudioBook, getString(R.string.section_title_audiobook));
+                    break;
+            }
+
+            return true;
+        };
     }
 
     private void loadPages() {
@@ -35,10 +75,31 @@ public class MainActivity extends AppCompatActivity {
         mBackendService.getPages()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(pagesList -> {
-                    Log.e("Hey!", pagesList.get(0).getMainText());
-                }, throwable -> {
+                .subscribe(this::showData, throwable -> {
                     Log.e(TAG, throwable.getLocalizedMessage());
                 });
+    }
+
+    private void showData(List<Page> pages) {
+        mFree = pages.get(0);
+        mDiscounted = pages.get(1);
+        mAudioBook = pages.get(2);
+
+        showPage(mFree, getString(R.string.section_title_free));
+    }
+
+    public void openReferralLink(View v) {
+        if (mReferralLink != null) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mReferralLink));
+            startActivity(intent);
+        }
+    }
+
+    private void showPage(Page page, String title) {
+        if (page != null) {
+            mDescription.setText(page.getMainText());
+            mReferralLink = page.getRedirectionUrl();
+            mPageTitle.setText(title);
+        }
     }
 }
