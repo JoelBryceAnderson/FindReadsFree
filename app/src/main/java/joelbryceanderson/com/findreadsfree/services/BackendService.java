1ddb943bbbd73a92498af424de81171a281071ec
Joel Anderson <joelbryceanderson@gmail.com>
2017-02-08 16:30:34 -0500

    changed return value to list

--- app/src/main/java/joelbryceanderson/com/findreadsfree/services/BackendService.java
+++ app/src/main/java/joelbryceanderson/com/findreadsfree/services/BackendService.java
@@ -1,6 +1,8 @@
 package joelbryceanderson.com.findreadsfree.services;
 
 
+import java.util.List;
+
 import io.reactivex.Observable;
 import joelbryceanderson.com.findreadsfree.models.Page;
 import retrofit2.http.GET;
@@ -13,6 +15,6 @@ public interface BackendService {
 
     String SERVICE_ENDPOINT = "http://s3.amazonaws.com";
 
-    @GET("/feed1/t5")
-    Observable<Page> getPages();
+    @GET("/feed1/t5.js")
+    Observable<List<Page>> getPages();
 }
