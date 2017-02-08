35b57cd14f64e5453d76e0e221d6200af4b48d1c
Joel Anderson <joelbryceanderson@gmail.com>
2017-02-08 16:52:59 -0500

    added adapter and converter factories

--- app/src/main/java/joelbryceanderson/com/findreadsfree/services/ServiceFactory.java
+++ app/src/main/java/joelbryceanderson/com/findreadsfree/services/ServiceFactory.java
@@ -1,6 +1,8 @@
 package joelbryceanderson.com.findreadsfree.services;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by JAnderson on 2/8/17.
@@ -13,6 +15,8 @@ public class ServiceFactory {
     public static <T> T createRetrofitService(final Class<T> clazz, final String endPoint) {
         final Retrofit restAdapter = new Retrofit.Builder()
                 .baseUrl(endPoint)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         return restAdapter.create(clazz);
