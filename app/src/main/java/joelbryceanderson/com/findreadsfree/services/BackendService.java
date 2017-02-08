f28f9d32186ec1230cb00c67bf0bb28df1a3fb42
Joel Anderson <joelbryceanderson@gmail.com>
2017-02-08 16:10:18 -0500

    added backend service

---
+++ app/src/main/java/joelbryceanderson/com/findreadsfree/services/BackendService.java
@@ -0,0 +1,18 @@
+package joelbryceanderson.com.findreadsfree.services;
+
+
+import io.reactivex.Observable;
+import joelbryceanderson.com.findreadsfree.models.Page;
+import retrofit2.http.GET;
+
+/**
+ * Created by JAnderson on 2/8/17.
+ */
+
+public interface BackendService {
+
+    String SERVICE_ENDPOINT = "http://s3.amazonaws.com";
+
+    @GET("/feed1/t5")
+    Observable<Page> getPages();
+}
