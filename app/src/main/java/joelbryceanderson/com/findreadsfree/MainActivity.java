f1ebec3900c5565dadbd7c5e0c551097f169c116
Joel Anderson <joelbryceanderson@gmail.com>
2017-02-08 16:30:56 -0500

    added initial loading of pages

--- app/src/main/java/joelbryceanderson/com/findreadsfree/MainActivity.java
+++ app/src/main/java/joelbryceanderson/com/findreadsfree/MainActivity.java
@@ -3,9 +3,17 @@ package joelbryceanderson.com.findreadsfree;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.widget.TextView;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import joelbryceanderson.com.findreadsfree.services.BackendService;
+import joelbryceanderson.com.findreadsfree.services.ServiceFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    BackendService mBackendService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = (TextView) findViewById(R.id.book_description);
         tv.setMovementMethod(new ScrollingMovementMethod());
+
+        loadPages();
+    }
+
+    private void loadPages() {
+        mBackendService = ServiceFactory.createRetrofitService(
+                BackendService.class, BackendService.SERVICE_ENDPOINT);
+
+        mBackendService.getPages()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(pagesList -> {
+                    Log.e("Hey!", pagesList.get(0).getMainText());
+                });
     }
 }
