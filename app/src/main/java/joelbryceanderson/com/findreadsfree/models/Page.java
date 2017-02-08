a86c1d4e4150d1c68122badd89604a18d9a4b3c7
Joel Anderson <joelbryceanderson@gmail.com>
2017-02-08 16:10:13 -0500

    added page model

---
+++ app/src/main/java/joelbryceanderson/com/findreadsfree/models/Page.java
@@ -0,0 +1,24 @@
+package joelbryceanderson.com.findreadsfree.models;
+
+/**
+ * Created by JAnderson on 2/8/17.
+ */
+
+public class Page {
+    
+    private String titleText;
+    private String mainText;
+    private String redirectionUrl;
+
+    public String getTitleText() {
+        return titleText;
+    }
+
+    public String getMainText() {
+        return mainText;
+    }
+
+    public String getRedirectionUrl() {
+        return redirectionUrl;
+    }
+}
